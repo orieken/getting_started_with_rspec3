56a02f778462858edbb525caa87a83b119ae8daf
Oscar Rieken <oscar.rieken@adp.com>
2017-03-27 00:18:29 -0400

    refactored sandwich to a let

--- spec/chapters/chapter_one/sandwich_spec.rb
+++ spec/chapters/chapter_one/sandwich_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 Sandwich = Struct.new(:taste, :toppings)
 
 describe 'An ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
+  let(:sandwich) { Sandwich.new('delicious', []) }
+  
   it 'is delicious' do
-    expect(@sandwich.taste).to eq('delicious')
+    expect(sandwich.taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    expect(@sandwich.toppings).not_to be_empty
+    sandwich.toppings << 'cheese'
+    expect(sandwich.toppings).not_to be_empty
   end
 end
