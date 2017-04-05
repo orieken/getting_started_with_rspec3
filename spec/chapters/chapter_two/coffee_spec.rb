84160bc55c51e69d194f8715122c281416f87c32
Oscar Rieken <oscar.rieken@adp.com>
2017-04-04 23:39:00 -0400

    starting chapter 2

---
+++ spec/chapters/chapter_two/coffee_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price).to eql(1.00)
+  end
+
+  context 'with milk' do
+    before { coffee.add :milk }
+
+    it 'costs $1.25' do
+      expect(coffee.price).to eql(1.25)
+    end
+  end
+end
