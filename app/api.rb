1af259f07ca89732ee26684edfe3771c3e1a6cc1
Oscar Rieken <oscar.rieken@adp.com>
2017-07-11 00:21:23 -0400

    added config and can now rackup

--- app/api.rb
+++ app/api.rb
@@ -7,6 +7,11 @@ module ExpenseTracker
   # ExpenseTracker API
   class API < Sinatra::Base
     post '/expense' do
+      JSON.generate('expense_id' => 42)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
     end
   end
 end
--- spec/chapters/chapter_four/expense_tracker_api_spec.rb
+++ spec/chapters/chapter_four/expense_tracker_api_spec.rb
@@ -5,13 +5,23 @@ require 'rack/test'
 require 'json'
 require_relative '../../../app/api'
 
-def app
-  ExpenseTracker::API.new
-end
-
 module ExpenseTracker
   describe 'Expense Tracker Api' do
     include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    def post_expense(expense)
+      post '/expense', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
 
     it 'records submitted expenses' do
       coffee = {
@@ -23,6 +33,34 @@ module ExpenseTracker
       post '/expense', JSON.generate(coffee)
       expect(last_response.status).to eq(200)
       expect(last_response).to be_ok
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+    end
+
+    it 'records submitted expenses' do
+      pending('need to persist data')
+      coffee = post_expense({
+                                payee: 'Starbucks',
+                                amount: 5.75,
+                                date: '2017-06-10'
+                            })
+      zoo = post_expense({
+                             payee: 'Zoo',
+                             amount: 15.25,
+                             date: '2017-06-10'
+                         })
+      groceries = post_expense({
+                                   payee: 'Whole Foods',
+                                   amount: 95.20,
+                                   date: '2017-06-11'
+                               })
+
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
   end
 end
+
